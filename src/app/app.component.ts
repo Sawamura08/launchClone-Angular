cac7b2860a542391de39ffb652530fde9000cc51
LinTech <hernandezmarkjulius24@gmail.com>
2024-03-02 20:59:37 +0800

    add hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private el: ElementRef, private render: Renderer2) {}
   title = 'launchClone';
+
+  isChecked: boolean = false;
+
+  try = () => {
+    const linkWrapper = this.el.nativeElement.querySelector('.link-wrapper');
+    const nav = this.el.nativeElement.querySelector('.nav');
+    const h3 = this.el.nativeElement.querySelector('.business');
+    this.isChecked = !this.isChecked;
+    if (this.isChecked) {
+      this.render.addClass(linkWrapper, 'animation-links');
+      this.render.setStyle(nav, 'background-color', 'white');
+      this.render.setStyle(h3, 'color', 'black');
+    } else {
+      this.render.removeClass(linkWrapper, 'animation-links');
+      this.render.setStyle(nav, 'background-color', 'transparent');
+      this.render.setStyle(h3, 'color', 'white');
+    }
+  };
 }
